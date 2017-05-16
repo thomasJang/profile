11348d761b4576e4c41b08b54b4cbf53986f4e66
thomasjang <tom@axisj.com>
2017-05-16 16:52:08 +0900

    update sample

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
 
-gulp.task('concat', function(){
 
-});
+function errorAlert(error) {
+    notify.onError({title: "Gulp Error", message: "Check your terminal", sound: "Purr"})(error); //Error Notification
+    console.log(error.toString());//Prints Error to Console
+    this.emit("end"); //End function
+}
 
 gulp.task('sass', function(){
   gulp.src('assets/scss/app.scss')
+      .pipe(plumber({errorHandler: errorAlert}))
       .pipe(sass({outputStyle: 'compressed'}))
       .pipe(gulp.dest('assets'));
 });
